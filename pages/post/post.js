7d985a63450c13ae302186f490d36e3761eab2b4
Songzhiguo <Fu_shide@163.com>
2018-02-11 16:16:52 +0800

    轮播图跳转详情页

--- pages/post/post.js
+++ pages/post/post.js
@@ -15,6 +15,12 @@ Page({
       url: 'post-detail/post-detail?id=' + postId
     });
   },
+  onSwiperTap: function (event) {
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
